49863c808b1852d0dcb7ab5d71859da9087925f9
Mateo Alejandro Mora <mateomora2015@gmail.com>
2025-02-19 11:25:31 -0500

    Ejercicio donde se pide una palabra y se imprime 10 veces

---
+++ MEnsajePorPantalla.py
@@ -0,0 +1,10 @@
+"""
+Escribir un programa que pida al usuario una palabra yla muestre por pantalla 10 veces
+"""
+
+palabra = input("\n***Digite una palabra: ")
+
+print("")
+for i in range(10):
+    print("| "+palabra)
+print("\n!Fin del proceso¡")

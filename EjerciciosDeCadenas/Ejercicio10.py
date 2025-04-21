5037c171f918fb7a51f6bdb2a735722b42cfb142
Mateo Alejandro Mora <mateomora2015@gmail.com>
2025-04-21 10:43:24 -0500

    Se agrego segunda forma de resolver en Ejercicio10

--- EjerciciosDeCadenas/Ejercicio10.py
+++ EjerciciosDeCadenas/Ejercicio10.py
@@ -1,4 +1,5 @@
 """Escribir un programa que pregunte por consola por los productos de una cesta de la compra, separados por comas, y muestre por pantalla cada 
 uno de los productos en una línea distinta."""
 compra = input("Dígite los productos de la compra separadas por comas: ")
-print(compra.replace(',', '\n'))
+#print(compra.replace(',', '\n')) Primera forma
+print('\n'.join(compra.split(','))) #Segunda forma

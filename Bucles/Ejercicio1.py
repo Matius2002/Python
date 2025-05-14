4d34f340f9bf99dff84d3f0bc537d19d2bb28122
Mateo Alejandro Mora <mateomora2015@gmail.com>
2025-05-14 10:07:32 -0500

    Ejercicio 1 de bucles hoy miercoles 14 de mayo de 2025

---
+++ Bucles/Ejercicio1.py
@@ -0,0 +1,6 @@
+"""Escribir un programa que pida al usuario una palabra y le muestre por pantalla 10 veces."""
+print("--------------INICIO DEL PROGRAMA--------------")
+palabra = input("Dígite una palabra: ")
+for i in range(0,10):
+    print(palabra)
+print("--------------FIN DEL PROGRAMA--------------")
